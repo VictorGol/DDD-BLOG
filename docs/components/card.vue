7f2c9b7c865dbee8423a6cb748092aee5fdb2b72
denghaoran <18843011017@qq.com>
2023-02-18 00:05:26 +0800

    1

--- docs/components/card.vue
+++ docs/components/card.vue
@@ -6,35 +6,17 @@ defineProps<{
 }>();
 
 const colors: string[] = [
-  "rgba(245, 245, 245, .5)",
-  "rgba(255, 250, 240, .5)",
-  "rgba(250, 240, 230, .5)",
+  "rgba(245, 245, 245, 0.5)",
+  "rgba(255, 250, 240, 0.5)",
+  "rgba(250, 240, 230, 0.5)",
 ];
 
 const bac: Ref<string> = ref(colors[Math.floor(Math.random() * 3)]);
 const elMouseover = (flag: boolean) => {
-  if (flag) {
-    bac.value = bac.value.replace(".5", "1");
-  } else {
-    bac.value = bac.value.replace("1", ".5");
-  }
+  bac.value = flag
+    ? bac.value.replace("0.5", "1")
+    : bac.value.replace("1", "0.5");
 };
-
-const timer = ref();
-const copyStatus = ref(false);
-// 复制文本到粘贴板
-const copyText = (val: string) => {
-  // navigator.clipboard.writeText(val).then(() => {
-  //   copyStatus.value = true;
-  //   timer.value = setTimeout(() => {
-  //     copyStatus.value = false;
-  //   }, 1000);
-  // });
-};
-
-onUnmounted(() => {
-  timer.value && clearTimeout(timer.value);
-});
 </script>
 
 <template>
@@ -44,11 +26,7 @@ onUnmounted(() => {
     @mouseover="elMouseover(true)"
     @mouseleave="elMouseover(false)"
   >
-    <div class="text" v-html="text" @click="copyText(text)"></div>
-    <!-- 过渡 -->
-    <!-- <Transition>
-      <div class="tip" v-if="copyStatus">复制成功</div>
-    </Transition> -->
+    <div class="text" v-html="text"></div>
   </div>
 </template>
 
@@ -66,31 +44,9 @@ onUnmounted(() => {
   .text {
     cursor: pointer;
   }
-  // .tip {
-  //   position: absolute;
-  //   top: 0;
-  //   right: 0;
-  //   bottom: 0;
-  //   left: 0;
-  //   display: flex;
-  //   justify-content: center;
-  //   align-items: center;
-  //   background-color: rgba($color: #fafafa, $alpha: 1);
-  //   border-radius: 12px;
-  // }
 }
 
 .wrap:hover {
   box-shadow: 1px 1px 3px 0px rgba(36, 36, 36, 0.6);
-}
-
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 0.5s ease;
-}
-
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
 }
 </style>
